c135635deed0a33caa91e77fd0860c35736191f1
yarutiun <y.arutiunofff@gmail.com>
2023-08-21 15:56:11 +0200

    done

--- c++07/ex02/Array.hpp
+++ c++07/ex02/Array.hpp
@@ -2,4 +2,68 @@
 #define ARRAY_HPP
 #include <iostream>
 
+template <class T> class Array
+{
+private:
+    T *_arr;
+    size_t _nSize;
+
+public:
+    
+    size_t size() const
+    {
+        return(_nSize);
+    }
+
+    Array(): _arr(NULL), _nSize(0)
+    {
+
+    }
+
+    const Array &operator=(const Array &a)
+    {
+        for(int i = 0; i < a._nSize; i++)
+            _arr[i] = a._arr[i];
+        _nSize = a._nSize;
+    }
+
+    Array(const Array &a)
+    {
+		_nSize = a.size();
+		_arr = new T[_nSize];
+		for (unsigned int i = 0; i < _nSize; ++i)
+			_arr[i] = a._arr[i];
+    }
+
+    Array(unsigned int n)
+    {
+        _arr = new T [n];
+        _nSize = n;
+    }
+
+
+    T &operator[](unsigned int n)
+    {
+        if(n >= _nSize)
+            throw outOfBounds();
+        return(_arr[n]);
+    }
+
+    ~Array()
+    {
+        if(_nSize)
+            delete [] _arr;
+        std::cout << "Destructor called" << std::endl;
+    }
+
+    class outOfBounds: public std::exception {
+        public:
+            virtual const char *what() const throw()
+            {
+               return("Out of bounds bruh"); 
+            }
+    }; 
+};
+
+
 #endif
--- c++07/ex02/main.cpp
+++ c++07/ex02/main.cpp
@@ -1,10 +1,49 @@
-#include "iter.hpp"
+#include "Array.hpp"
+#define MAX_VAL 100
 
 int main(void)
 {
-	int array_ints[] = {1, 2, 3, 4};
-	::iter<int>(array_ints, 4, ::print);
+	Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
 
-	std::string string = "Im iterating lol";
-	iter<const char>(string.c_str(), string.length(), ::print);
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;
+    return 0;
 }
