3b2f58117d5b0d8827762b2dd8f4f9b558fdabe5
yarutiun <yarutiun@student.42wolfsburg.de>
2023-08-30 13:56:25 +0200

    just started

---
+++ c++09/ex00/main.cpp
@@ -0,0 +1,10 @@
+#include "BitcoinExchange.hpp"
+
+int main(int argc, char **argv)
+{
+    if(argc != 2)
+    {
+        std::cout << "Wrong input to your programm (should only accept one file)" << std::endl;
+        return(0);
+    }
+}
